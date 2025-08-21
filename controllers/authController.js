b3c86c6a43aeac5f5a7b68a76d5722aadf11f094
Hawasandra1 <hawasandra.m@gmail.com>
2025-08-21 13:26:13 +0300

    Fixed the employee.js model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+const { User, Employee } = require('../models');
 
 // Generate JWT Token
 const generateToken = (userId) => {
